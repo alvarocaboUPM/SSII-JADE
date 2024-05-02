c9cd49b731e598a63a91b739144cad4483b2cf92
Alvaro <alvarocaboac2@gmail.com>
2024-05-02 22:58:14 +0200

    Added some docs

--- src/models/agents.py
+++ src/models/agents.py
@@ -22,13 +22,39 @@ class AgentPublisher(Agent):
         self.behaviours.append(self.timed)
 
 class AgentSubscriber(Agent):
+    """
+    Represents a subscriber agent that can launch a subscriber protocol.
+
+    Args:
+        aid (str): The unique identifier of the agent.
+        receiver_aid (AID): The AID of the receiver agent.
+
+    Attributes:
+        protocol (SubscriberProtocol): The subscriber protocol associated with the agent.
+
+    """
 
     def __init__(self, aid, receiver_aid: AID):
+        """
+        Initializes an instance of the AgentSubscriber class.
+
+        Args:
+            aid (str): The identifier of the agent.
+            receiver_aid (AID): The identifier of the receiver agent.
+
+        """
         super(AgentSubscriber, self).__init__(aid)
 
         self.call_later(8.0, self.launch_subscriber_protocol, receiver_aid)
 
     def launch_subscriber_protocol(self, aid: AID):
+        """
+        Launches the subscriber protocol.
+
+        Args:
+            aid (AID): The AID of the receiver agent.
+
+        """
         msg = ACLMessage(ACLMessage.SUBSCRIBE)
         msg.set_protocol(ACLMessage.FIPA_SUBSCRIBE_PROTOCOL)
         msg.set_content('Subscription request')
